220fee429467acc6d471a194487ae7ce3a95d255
Mr.Blue <silverhugh.77@gmail.com>
2019-07-09 20:54:57 +0800

    Update openwrt.sh

--- scripts/openwrt.sh
+++ scripts/openwrt.sh
@@ -6,3 +6,5 @@ opkg update
 opkg install kmod-ipt-nat6
 ip6tables -t nat -I POSTROUTING -s `uci get network.globals.ula_prefix` -j MASQUERADE
 route -A inet6 add 2000::/3 `route -A inet6 | grep ::/0 | awk 'NR==1{print "gw "$2" dev "$7}'`
+
+# Shadowsocks - https://alalin.me/archives/805
