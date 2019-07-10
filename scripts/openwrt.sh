b68b8189fa3c8d27057b9d4877932845f1d9bb8a
Mr.Blue <silverhugh.77@gmail.com>
2019-07-10 11:32:51 +0800

    Update openwrt.sh

--- scripts/openwrt.sh
+++ scripts/openwrt.sh
@@ -8,3 +8,8 @@ ip6tables -t nat -I POSTROUTING -s `uci get network.globals.ula_prefix` -j MASQU
 route -A inet6 add 2000::/3 `route -A inet6 | grep ::/0 | awk 'NR==1{print "gw "$2" dev "$7}'`
 
 # Shadowsocks - https://alalin.me/archives/805
+crontab -e
+# <minute> <hour> <day of month> <month> <day of week> <command>
+# 0 5 5 * *    wget --no-check-certificate https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt -O /tmp/china_ip_list.txt && mv /tmp/china_ip_list.txt /etc/chinadns_chnroute.txt
+/etc/init.d/cron start
+/etc/init.d/cron enable
