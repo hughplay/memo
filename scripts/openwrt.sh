83df97315950547498043cb22e5374e3e8265c88
Mr.Blue <silverhugh.77@gmail.com>
2019-07-10 11:33:17 +0800

    Update openwrt.sh

--- scripts/openwrt.sh
+++ scripts/openwrt.sh
@@ -9,6 +9,7 @@ route -A inet6 add 2000::/3 `route -A inet6 | grep ::/0 | awk 'NR==1{print "gw "
 
 # Shadowsocks - https://alalin.me/archives/805
 crontab -e
+# m h  dom mon dow   command
 # <minute> <hour> <day of month> <month> <day of week> <command>
 # 0 5 5 * *    wget --no-check-certificate https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt -O /tmp/china_ip_list.txt && mv /tmp/china_ip_list.txt /etc/chinadns_chnroute.txt
 /etc/init.d/cron start
