e03ad566b8fd32482d519a2809cd8b11431b1fb6
Mr.Blue <silverhugh.77@gmail.com>
2019-07-15 14:58:52 +0800

    Update openwrt.sh

--- scripts/openwrt.sh
+++ scripts/openwrt.sh
@@ -6,15 +6,32 @@
 # Baidu  - 180.76.76.76
 # Tecent - 119.29.29.29 √
 
+
 # TUNA openwrt
 sed -i 's/downloads.openwrt.org/mirrors.tuna.tsinghua.edu.cn\/lede/g' /etc/opkg/distfeeds.conf
-opkg update
 
-# IPV6 - https://windard.com/blog/2018/06/18/Openwrt-Ipv6
-opkg install kmod-ipt-nat6
-# run & write two lines below into `/etc/rc.local`
-ip6tables -t nat -I POSTROUTING -s `uci get network.globals.ula_prefix` -j MASQUERADE
-route -A inet6 add 2000::/3 `route -A inet6 | grep ::/0 | awk 'NR==1{print "gw "$2" dev "$7}'`
+
+# IPV6 - https://github.com/tuna/ipv6.tsinghua.edu.cn/blob/master/openwrt.md
+opkg update && opkg install ip6tables kmod-ipt-nat6 kmod-ip6tables kmod-ip6tables-extra luci-proto-ipv6 iputils-traceroute6
+# IPv6 ULA-Prefix fd0a:f047:3583::/64
+# Network -> Interfaces -> LAN -> DHCP Server -> check `Always announce default router`
+
+# /etc/firewall.user
+WAN6=eth0.2
+LAN=br-lan
+ip6tables -t nat -A POSTROUTING -o $WAN6 -j MASQUERADE
+ip6tables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+ip6tables -A FORWARD -i $LAN -j ACCEPT
+
+# /etc/hotplug.d/iface/99-ipv6
+#!/bin/sh
+[ "$ACTION" = ifup ] || exit 0
+iface=wan6
+[ -z "$iface" -o "$INTERFACE" = "$iface" ] || exit 0
+ip -6 route add `ip -6 route show default|sed -e 's/from [^ ]* //'`
+logger -t IPv6 "Add IPv6 default route."
+
+chmod +x /etc/hotplug.d/iface/99-ipv6
 
 # Shadowsocks - https://alalin.me/archives/805
 crontab -e
