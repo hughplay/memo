f2b1590ae26ba457ea57f0b2af3eef16a14be3de
Mr.Blue <silverhugh.77@gmail.com>
2019-07-15 13:57:09 +0800

    Update openwrt.sh

--- scripts/openwrt.sh
+++ scripts/openwrt.sh
@@ -12,12 +12,9 @@ opkg update
 
 # IPV6 - https://windard.com/blog/2018/06/18/Openwrt-Ipv6
 opkg install kmod-ipt-nat6
+# run & write two lines below into `/etc/rc.local`
 ip6tables -t nat -I POSTROUTING -s `uci get network.globals.ula_prefix` -j MASQUERADE
 route -A inet6 add 2000::/3 `route -A inet6 | grep ::/0 | awk 'NR==1{print "gw "$2" dev "$7}'`
-# LAN -> DHCP Server -> IPV6 Setting
-# disable `DHCPv6-Service`
-# check `Always announce default router`
-# Done
 
 # Shadowsocks - https://alalin.me/archives/805
 crontab -e
