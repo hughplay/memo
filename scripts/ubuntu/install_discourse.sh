e372cb3b54f51ce476f543f60e96e4f80ad0a57e
Mr.Blue <silverhugh.77@gmail.com>
2019-08-20 10:30:06 +0800

    Create install_discourse.sh

---
+++ scripts/ubuntu/install_discourse.sh
@@ -0,0 +1,10 @@
+# https://github.com/discourse/discourse/blob/master/docs/INSTALL-cloud.md
+
+# Domain name
+# namecheap: education.github.com/pack
+
+# Email Server
+# Mailjet: https://www.mailjet.com/
+# My account -> Senders & Domains (do 3 part one by one)
+# !Important: If you own `example.com`, please 
+# (Maybe 网易企业邮箱: http://ym.163.com)
