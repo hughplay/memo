9b16031e36a94dd5b4b9195a148e6ccea796cdbf
Mr.Blue <silverhugh.77@gmail.com>
2019-09-14 00:56:22 +0800

    Update install_discourse.sh

--- scripts/ubuntu/install_discourse.sh
+++ scripts/ubuntu/install_discourse.sh
@@ -3,6 +3,9 @@
 # Domain name
 # namecheap: education.github.com/pack
 
+# change timezone to UTC
+sudo timedatectl set-timezone UTC
+
 # Email Server
 # Mailjet: https://www.mailjet.com/
 # My account -> Senders & Domains (do 3 part one by one)
@@ -14,6 +17,10 @@
 # - git clone https://github.com/discourse/discourse-math.git     
 # - git clone https://github.com/discourse/discourse-solved.git   
 # - git clone https://github.com/discourse/discourse-checklist.git
+
+# Create admin
+./launcher enter app
+rake admin:create
 
 crontab -e
 0 0,12 * * * /var/discourse/shared/standalone/scripts/update_score.py
