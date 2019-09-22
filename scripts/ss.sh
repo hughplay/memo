bd0f85cef073eef2e2537551e124df57c15d5823
Mr.Blue <silverhugh.77@gmail.com>
2019-09-22 14:46:58 +0800

    Update ss.sh

--- scripts/ss.sh
+++ scripts/ss.sh
@@ -11,3 +11,11 @@ chmod +x shadowsocksR.sh
 
 # Windows: https://github.com/shadowsocksrr/shadowsocksr-csharp/releases
 # OS X: https://github.com/shadowsocks/shadowsocks-iOS/wiki/Shadowsocks-for-OSX-Help
+
+
+# V2Ray
+# https://github.com/233boy/v2ray/wiki/V2Ray%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85%E8%84%9A%E6%9C%AC
+bash <(curl -s -L https://git.io/v2ray.sh)
+
+# client: https://www.v2ray.com/awesome/tools.html
+# Mac OS X: https://github.com/Cenmrev/V2RayX
