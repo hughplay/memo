c2c1e898aa172389a760924b979840dba95ee489
Mr.Blue <silverhugh.77@gmail.com>
2020-02-09 00:42:15 +0800

    Update and rename ss.sh to ilovegfw.sh

--- scripts/ilovegfw.sh
+++ scripts/ilovegfw.sh
@@ -12,6 +12,7 @@ chmod +x shadowsocksR.sh
 # Windows: https://github.com/shadowsocksrr/shadowsocksr-csharp/releases
 # OS X: https://github.com/shadowsocks/shadowsocks-iOS/wiki/Shadowsocks-for-OSX-Help
 
+# ===
 
 # V2Ray
 # https://github.com/233boy/v2ray/wiki/V2Ray%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85%E8%84%9A%E6%9C%AC
@@ -19,3 +20,6 @@ bash <(curl -s -L https://git.io/v2ray.sh)
 
 # client: https://www.v2ray.com/awesome/tools.html
 # Mac OS X: https://github.com/Cenmrev/V2RayX
+
+# WebSocket+TLS+Web: https://toutyrater.github.io/advanced/wss_and_web.html
+# Why works better: https://www.youtube.com/watch?v=zlOTrR1AzpA
