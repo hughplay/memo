48b09029f0d34c326500990fd81473f54960802f
Mr.Blue <silverhugh.77@gmail.com>
2020-03-23 14:07:19 +0800

    Update install.sh

--- scripts/windows/install.sh
+++ scripts/windows/install.sh
@@ -1,13 +1,20 @@
 # == Best Terminal in Windows ==
 
-# Install hyper.js as Terminal (Best choice!): https://hyper.is/
-# Install drcula theme with config file: https://draculatheme.com/hyper
+# 1. Install a WSL distribution
 # If you don't have windows store, install it with: https://github.com/kkkgo/LTSC-Add-MicrosoftStore
 # Enable Windows Subsystem for Linux (WSL) in PowerShell with admistration's permission
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 # Install a Linux Distribution from Windows Store, e.g: Ubuntu 18.04 LTS
+
+# 2. Install Hyper.js
+# Install hyper.js as Terminal (Best choice!): https://hyper.is/
+# Install drcula theme with config file: https://draculatheme.com/hyper
 # Make Hyper launch with WSL: Edit -> Preferences,
 #   shell: 'C:\\Windows\\System32\\bash.exe'
+# If you prefer to use ZSH
+#   shell: 'C:\\Windows\\System32\\wsl.exe',
+#   shellArgs: [],
+
 # True Color Detection: https://gist.github.com/XVilka/8346728
 printf "\x1b[${bg};2;${red};${green};${blue}m\n"
 # Install Powerline font, e.g. Monaco for Powerline: https://github.com/hughplay/fonts
