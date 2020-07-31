d910730b841e15b327a92b802cb09f9ca4ccf942
Mr.Blue <silverhugh.77@gmail.com>
2020-07-31 21:14:54 +0800

    Update install_overleaf.sh

--- scripts/ubuntu/install_overleaf.sh
+++ scripts/ubuntu/install_overleaf.sh
@@ -7,7 +7,7 @@ sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-c
 sudo chmod +x /usr/local/bin/docker-compose
 
 # First time
-docker-compose up
+docker-compose up -d
 docker exec sharelatex tlmgr update --self --repository https://mirror.bjtu.edu.cn/ctan/systems/texlive/tlnet
 docker exec sharelatex tlmgr install scheme-full --repository https://mirror.bjtu.edu.cn/ctan/systems/texlive/tlnet
 # TUNA: https://mirrors.tuna.tsinghua.edu.cn/CTAN/systems/texlive/tlnet
