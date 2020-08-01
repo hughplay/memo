1fc60b2c7fe2dfdfbaabec6ab72b5e47aaf6eef9
Mr.Blue <silverhugh.77@gmail.com>
2020-08-01 15:53:14 +0800

    Create mirrors.sh

---
+++ scripts/mirrors.sh
@@ -0,0 +1,37 @@
+## pypi
+# https://mirrors.tuna.tsinghua.edu.cn/help/pypi/
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
+## conda
+# https://mirrors.tuna.tsinghua.edu.cn/help/anaconda/
+# Miniconda: https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/
+vim ~/.condarc
+
+channels:
+  - defaults
+show_channel_urls: true
+channel_alias: https://mirrors.tuna.tsinghua.edu.cn/anaconda
+default_channels:
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/r
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/pro
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/msys2
+custom_channels:
+  conda-forge: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  msys2: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  bioconda: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  menpo: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  pytorch: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  simpleitk: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  
+## ubuntu
+# https://mirrors.tuna.tsinghua.edu.cn/help/ubuntu/
+  
+## raspberry pi
+# `/etc/apt/sources.list`
+deb http://mirrors.tuna.tsinghua.edu.cn/raspberry-pi-os/raspbian/ buster main non-free contrib rpi
+deb-src http://mirrors.tuna.tsinghua.edu.cn/raspberry-pi-os/raspbian/ buster main non-free contrib rpi
+
+# `/etc/apt/sources.list.d/raspi.list`
+deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ buster main ui
