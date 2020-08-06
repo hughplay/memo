8a6009b31568920524c0d8cdcfd206a9f1c5879c
Mr.Blue <silverhugh.77@gmail.com>
2020-08-06 13:02:28 +0800

    Update install-discourse.sh

--- scripts/centos/install-discourse.sh
+++ scripts/centos/install-discourse.sh
@@ -1,35 +1,13 @@
 # Install Docker
-sudo yum remove docker \
-                  docker-client \
-                  docker-client-latest \
-                  docker-common \
-                  docker-latest \
-                  docker-latest-logrotate \
-                  docker-logrotate \
-                  docker-engine
+# https://mirrors.tuna.tsinghua.edu.cn/help/docker-ce/
 
-sudo yum install -y yum-utils \
-  device-mapper-persistent-data \
-  lvm2
-
-sudo yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
-
-sudo yum install -y docker-ce docker-ce-cli containerd.io
-
-# modify repo mirror
+# modify repo mirror (optional)
 # https://lug.ustc.edu.cn/wiki/mirrors/help/docker
 sudo systemctl start docker
-
-# docker --version
-# Docker version 19.03.1, build 74b1e89
 
 # Install Discourse
 sudo -s
 git clone https://github.com/discourse/discourse_docker.git /var/discourse
 cd /var/discourse
-
-# https://meta.discoursecn.org/t/topic/2659/2
-# mlbrain.com
-# mx.ym.163.com
+# edit app.yml
+./launcher rebuild app
