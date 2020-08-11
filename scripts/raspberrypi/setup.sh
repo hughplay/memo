a3c11a25b92847ea61f0de6b91790c17ab505599
Mr.Blue <silverhugh.77@gmail.com>
2020-08-11 22:59:50 +0800

    Create setup.sh

---
+++ scripts/raspberrypi/setup.sh
@@ -0,0 +1,14 @@
+## Enable SSH
+# https://howchoo.com/g/ote0ywmzywj/how-to-enable-ssh-on-raspbian-without-a-screen
+touch /boot/ssh
+## Connect to Wifi
+# https://howchoo.com/g/ndy1zte2yjn/how-to-set-up-wifi-on-your-raspberry-pi-without-ethernet
+# /boot/wpa_supplicant.conf
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=CN
+
+network={
+	ssid="<WIFI NAME>"
+	psk="<WIFI PASSWORD>"
+}
