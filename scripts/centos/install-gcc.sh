a46bdf787d8df204952ce327417ad091911677bf
Mr.Blue <silverhugh.77@gmail.com>
2020-11-05 12:19:55 +0800

    Create install-gcc.sh

---
+++ scripts/centos/install-gcc.sh
@@ -0,0 +1,11 @@
+# https://gcc.gnu.org/mirrors.html
+# https://gcc.gnu.org/wiki/InstallingGCC
+tar xzf gcc-4.6.2.tar.gz
+cd gcc-4.6.2
+./contrib/download_prerequisites
+cd ..
+mkdir objdir
+cd objdir
+$PWD/../gcc-4.6.2/configure --prefix=$HOME/GCC-4.6.2 --enable-languages=c,c++,fortran,go
+make
+make install
