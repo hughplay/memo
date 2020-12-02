596619dbf00960580438edb3a2702eba59d5b5c5
Mr.Blue <silverhugh.77@gmail.com>
2020-12-02 15:52:55 +0800

    Update install-gcc.sh

--- scripts/centos/install-gcc.sh
+++ scripts/centos/install-gcc.sh
@@ -1,16 +1,38 @@
-# https://gcc.gnu.org/mirrors.html https://mirrors.ustc.edu.cn/gnu/gcc/
-# https://gcc.gnu.org/wiki/InstallingGCC
+#!/usr/bin/env bash
+set -e
 
-wget https://mirrors.ustc.edu.cn/gnu/gcc/gcc-6.5.0/ -O /tmp/gcc-6.5.0
-tar xzf gcc-4.6.2.tar.gz
-cd gcc-4.6.2
+VERSION=${version:-6.5.0}
+PREFIX=${prefix:-/usr/local/}
+JOB=${job:-`nproc`}
+
+START=`pwd`
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -v|--version) VERSION="$2"; shift ;;
+        -p|--prefix) PREFIX="$2"; shift ;;
+        -j|--job) JOB="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+echo "Downloading gcc-${VERSION}...""
+cd /tmp
+wget https://mirrors.ustc.edu.cn/gnu/gcc/gcc-${VERSION}/gcc-${VERSION}.tar.gz -O gcc-${VERSION}.tar.gz
+echo 'Decompressing into gcc-${VERSION}...'
+tar xzf gcc-${VERSION}.tar.gz
+cd gcc-${VERSION}
+
+echo "Downloading dependencies..."
 ./contrib/download_prerequisites
-# cd ..
-# mkdir objdir
-# cd objdir
-# $PWD/../gcc-4.6.2/configure --prefix=$HOME/GCC-4.6.2 --enable-languages=c,c++,fortran,go --disable-multilib
+
+echo "Building..."
 mkdir build
 cd build
-./configure --disable-multilib
-make
+../configure --disable-multilib --prefix $PREFIX
+make -j $JOB
 make install
+
+cd $START
+echo '$PREFIX/bin/gcc is installed.'
