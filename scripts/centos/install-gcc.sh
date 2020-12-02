84c0c4214996dcba1623fed7470bdf63999131b1
Mr.Blue <silverhugh.77@gmail.com>
2020-12-02 11:06:52 +0800

    Update install-gcc.sh

--- scripts/centos/install-gcc.sh
+++ scripts/centos/install-gcc.sh
@@ -1,5 +1,7 @@
 # https://gcc.gnu.org/mirrors.html https://mirrors.ustc.edu.cn/gnu/gcc/
 # https://gcc.gnu.org/wiki/InstallingGCC
+
+wget https://mirrors.ustc.edu.cn/gnu/gcc/gcc-6.5.0/ -O /tmp/gcc-6.5.0
 tar xzf gcc-4.6.2.tar.gz
 cd gcc-4.6.2
 ./contrib/download_prerequisites
@@ -7,6 +9,8 @@ cd gcc-4.6.2
 # mkdir objdir
 # cd objdir
 # $PWD/../gcc-4.6.2/configure --prefix=$HOME/GCC-4.6.2 --enable-languages=c,c++,fortran,go --disable-multilib
+mkdir build
+cd build
 ./configure --disable-multilib
 make
 make install
