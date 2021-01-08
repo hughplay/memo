daa9840af4d9e5b06703eeb096ecf4dd4cc7d5c7
Mr.Blue <silverhugh.77@gmail.com>
2021-01-08 17:15:02 +0800

    Update docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,2 +1,2 @@
 # docker build over a proxy
-docker build --build-arg http_proxy=http://10.61.2.216:8001 --build-arg https_proxy=http://10.61.2.216:8001 -t <tag> .
+docker build --build-arg http_proxy=http://127.0.0.1:8001 --build-arg https_proxy=http://127.0.0.1:8001 -t <tag> .
