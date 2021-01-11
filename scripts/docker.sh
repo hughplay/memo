6848727b408c3febf393a032b7de3b611901004f
Mr.Blue <silverhugh.77@gmail.com>
2021-01-11 14:46:28 +0800

    Update docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,2 +1,13 @@
 # docker build over a proxy
 docker build --build-arg http_proxy=http://127.0.0.1:8001 --build-arg https_proxy=http://127.0.0.1:8001 -t <tag> .
+# docker compose: build -> args
+
+# export and import docker image
+# https://stackoverflow.com/questions/23935141/how-to-copy-docker-images-from-one-host-to-another-without-using-a-repository
+docker save -o <xxx.tar> <image name>
+docker load -i <xxx.tar>
+
+# make container keep running, Dockerfile:
+CMD ['sleep', 'infinity']
+# docker compose
+command ['sleep', 'infinity']
