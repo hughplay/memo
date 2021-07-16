1ff628115aa4c9dba6212fe5749458e94b474f51
Mr.Blue <silverhugh.77@gmail.com>
2021-07-16 11:46:34 +0800

    Update ilovegfw.sh

--- scripts/ilovegfw.sh
+++ scripts/ilovegfw.sh
@@ -17,9 +17,26 @@ chmod +x shadowsocksR.sh
 # V2Ray
 # https://github.com/233boy/v2ray/wiki/V2Ray%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85%E8%84%9A%E6%9C%AC
 bash <(curl -s -L https://git.io/v2ray.sh)
+# New: https://www.v2ray.com/chapter_00/install.html
+bash <(curl -L -s https://install.direct/go.sh)
 
 # client: https://www.v2ray.com/awesome/tools.html
 # Mac OS X: https://github.com/Cenmrev/V2RayX
 
 # WebSocket+TLS+Web: https://toutyrater.github.io/advanced/wss_and_web.html
 # Why works better: https://www.youtube.com/watch?v=zlOTrR1AzpA
+
+# === Github
+# 1. Using cnpmjs.org
+git clone https://github.com.cnpmjs.org/hughplay/memo
+# 2. Change hosts： https://github.com/ovenx/github-hosts
+
+# === Package Server
+# TUNA: https://mirrors.tuna.tsinghua.edu.cn/
+#    - conda: https://mirrors.tuna.tsinghua.edu.cn/help/anaconda/
+#    - Ubuntu: https://mirrors.tuna.tsinghua.edu.cn/help/ubuntu/
+#    - Yum: https://mirrors.tuna.tsinghua.edu.cn/help/centos/
+# NPM: Use yrm - https://www.npmjs.com/package/yrm/v/1.0.6?activeTab=readme
+npm install -g yrm
+yrm test
+yrm use taobao
