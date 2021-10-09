658f52f2d1969d14f9330c1e656aa34448847888
Mr.Blue <silverhugh.77@gmail.com>
2021-10-09 10:34:19 +0800

    Update ssh.sh

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,4 +1,4 @@
-# local --> middle -> target
+# Basic jump: local -> middle -> target
 Host middle
     User xx
     Hostname x.x.x.x
@@ -7,5 +7,34 @@ Host target
     User xx
     Hostname x.x.x.x
     ProxyCommand ssh middle nc %h %p
+
+ssh target
+
+# Basic jump with proxy: local -> middle -> (proxy) -> target
+Host middle
+    User xx
+    Hostname x.x.x.x
+
+Host target
+    User xx
+    Hostname x.x.x.x
+    ProxyCommand ssh middle nc --proxy-type http --proxy <address>:<port> %h %p
+
+ssh target
+
+# Multiple jump: local -> middle_a -> middle_b -> target
+Host middle_a
+    User xx
+    Hostname x.x.x.x
+
+Host middle_b
+    User xx
+    Hostname x.x.x.x
+    ProxyCommand ssh middle_a nc %h %p
+
+Host target
+    User xx
+    Hostname x.x.x.x
+    ProxyCommand ssh middle_b nc %h %p
 
 ssh target
