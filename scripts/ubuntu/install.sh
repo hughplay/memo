bb097b6a7910bf23cbc391cfd76572a2a53cd288
Mr.Blue <silverhugh.77@gmail.com>
2022-03-01 11:16:49 +0800

    Update install.sh

--- scripts/ubuntu/install.sh
+++ scripts/ubuntu/install.sh
@@ -24,50 +24,41 @@ sudo apt-get update && sudo apt-get install -y \
         tree
 
 # (Internet) Install tmux, checked
-git clone https://github.com/tmux/tmux.git /tmp/tmux \
-    && cd /tmp/tmux \
-    && git checkout tags/2.7 \
-    && sh autogen.sh \
-    && ./configure && make \
-    && sudo mv tmux /usr/bin/tmux \
-    && cd \
-    && rm -rf /tmp/tmux
+sudo bash <(curl -s https://raw.githubusercontent.com/hughplay/env/master/scripts/ubuntu/install_tmux_x.sh)
 
 # Use zsh as default, checked
 command -v zsh | sudo tee -a /etc/shells \
     && sudo chsh -s "$(command -v zsh)" "${USER}"
 
 # (Internet) Zsh config: 1. oh-my-zsh 2. honukai
-wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O /tmp/install-oh-my-zsh.sh \
-    && sed -i '/env zsh/d' /tmp/install-oh-my-zsh.sh \
-    && sed -i 's/chsh -s/sudo chsh -s/g' /tmp/install-oh-my-zsh.sh \
-    && sh /tmp/install-oh-my-zsh.sh \
+wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O /tmp/install-oh-my-zsh.sh --no-check-certificate \
+    && sh /tmp/install-oh-my-zsh.sh --unattended \
     && rm -f /tmp/install-oh-my-zsh.sh \
-    && wget https://raw.githubusercontent.com/oskarkrawczyk/honukai-iterm/master/honukai.zsh-theme -O ~/.oh-my-zsh/themes/honukai.zsh-theme \
-    && sed -i.bak '/ZSH_THEME/s/\".*\"/\"honukai\"/' ~/.zshrc
+    && wget https://raw.githubusercontent.com/oskarkrawczyk/honukai-iterm/master/honukai.zsh-theme -O ~/.oh-my-zsh/themes/honukai.zsh-theme --no-check-certificate \
+    && sed -i.bak '/ZSH_THEME/s/\".*\"/\"honukai\"/' ~/.zshrc\
+    && git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions\
+    && sed -i.bak '/plugin/s/(.*)/(git zsh-autosuggestions)/' ~/.zshrc
 
 # (Internet) Use localrc: common used alias
-wget https://raw.githubusercontent.com/hughplay/env/master/workbench/.localrc -O $HOME/.localrc \
-    && echo 'source $HOME/.localrc' >> $HOME/.zshrc
+# wget https://raw.githubusercontent.com/hughplay/env/master/workbench/.localrc -O $HOME/.localrc \
+#     && echo 'source $HOME/.localrc' >> $HOME/.zshrc
 
 # (ppa:neovim-ppa/stable, 502 Bad GateWay), checked
 # make sure you have set http_proxy and https_proxy correctly if you need to
 # or you will get `ERROR: '~neovim-ppa' user or team does not exist`.
-sudo apt-get install -y software-properties-common \
-    && sudo add-apt-repository -y ppa:neovim-ppa/unstable \
-    && sudo apt-get update \
-    && sudo apt-get install -y neovim \
-    && pip install neovim --user
+# sudo apt-get install -y software-properties-common \
+#     && sudo add-apt-repository -y ppa:neovim-ppa/unstable \
+#     && sudo apt-get update \
+#     && sudo apt-get install -y neovim \
+#     && pip install neovim --user
 
 # (Internet) Tmux config, checked
 git clone https://github.com/hughplay/tmux-config.git /tmp/tmux-config \
-    && zsh /tmp/tmux-config/install.sh \
+    && bash /tmp/tmux-config/install.sh \
     && rm -rf /tmp/tmux-config
 
-# Vim config: modified spf13-vim, checked
-curl https://raw.githubusercontent.com/hughplay/spf13-vim/3.0/bootstrap.sh -L -o - | bash \
-    && cd $HOME/.vim/bundle/YouCompleteMe \
-    && python install.py
+# Vim config: lightvim
+wget https://raw.githubusercontent.com/hughplay/lightvim/master/install.sh -O - | sh
 
 # ====================
 #   Useful Snippets
@@ -75,7 +66,7 @@ curl https://raw.githubusercontent.com/hughplay/spf13-vim/3.0/bootstrap.sh -L -o
 
 # > set timezone
 : <<'comment'
-ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+sudo ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 comment
 
 # > Vim to Nvim
@@ -116,3 +107,14 @@ curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
 comment
+
+# docker
+sudo addgroup --system docker
+sudo adduser $USER docker
+newgrp docker
+sudo snap disable docker
+sudo snap enable docker
+
+# mDNS
+# https://gist.github.com/davisford/5984768
+sudo apt-get install avahi-daemon avahi-discover avahi-utils libnss-mdns mdns-scan
