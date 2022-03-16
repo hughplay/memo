de2c464558f918e8de8062491caa114411adc852
Mr.Blue <silverhugh.77@gmail.com>
2022-03-16 16:16:28 +0800

    Update install-webdav.sh

--- scripts/ubuntu/install-webdav.sh
+++ scripts/ubuntu/install-webdav.sh
@@ -7,14 +7,38 @@ sudo systemctl restart apache2
 sudo vim /etc/apache2/ports.conf
 sudo vim /etc/apache2/sites-enabled/000-default.conf
 systemctl restart apache2
-#
-sudo a2enmod dav_f
+ 
 sudo a2enmod dav_fs
 sudo systemctl restart apache2
 sudo mkdir -p /var/www/webdav
 sudo chown www-data /var/www/webdav
 sudo vim /etc/apache2/sites-available/webdav.conf
+
+NameVirtualHost *
+<VirtualHost *:80>
+ServerAdmin webmaster@domain
+
+DocumentRoot /var/www/webdav/
+<Directory /var/www/webdav/>
+Options Indexes MultiViews
+AllowOverride None
+Order allow,deny
+allow from all
+</Directory>
+
+</VirtualHost>
+Alias /webdav /var/www/webdav
+<Location /webdav>
+DAV On
+AuthType Basic
+AuthName “webdav”
+AuthUserFile /var/www/webdav/passwd.dav
+Require valid-user
+</Location>
+
 sudo a2ensite webdav.conf
+
+# webdav username and password
 sudo adduser xxx
 sudo htpasswd -c /var/www/webdav/passwd.dav xxx
 sudo chown root:www-data /var/www/webdav/passwd.dav
