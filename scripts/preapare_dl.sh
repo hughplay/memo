00a53a861bc8830f32bb02b392c9c0201a65cea8
Mr.Blue <silverhugh.77@gmail.com>
2022-04-13 21:11:10 +0800

    Create preapare_dl.sh

---
+++ scripts/preapare_dl.sh
@@ -0,0 +1,167 @@
+# Prepare GPUs for deep learning
+
+
+# Add user
+sudo adduser xxx
+sudo passwd xxx
+sudo usermod -aG sudo xxx
+
+
+# Basic packages for Ubuntu
+# https://mirrors.tuna.tsinghua.edu.cn/help/ubuntu/
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo vim /etc/apt/sources.list
+""" Ubuntu 20.04
+# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse
+
+# 预发布软件源，不建议启用
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse
+"""
+sudo apt-get update && sudo apt-get install -y --allow-downgrades --allow-change-held-packages --no-install-recommends \
+    build-essential \
+    cmake \
+    g++-7 \
+    git \
+    curl \
+    vim \
+    wget \
+    ca-certificates \
+    libjpeg-dev \
+    libpng-dev \
+    librdmacm1 \
+    libibverbs1 \
+    ibverbs-providers \
+    zsh \
+    tzdata \
+    libgl1-mesa-glx \
+    libglib2.0-0
+    zip \
+    unzip \
+    rsync \
+    htop \
+    language-pack-en \
+    nethogs \
+    sysstat \
+    gnupg \
+    lsb-release
+
+
+# Install v2ray as you may need :)
+# https://github.com/v2fly/v2ray-core/releases/latest
+wget https://hub.fastgit.xyz/v2fly/v2ray-core/releases/download/v4.44.0/v2ray-linux-64.zip
+unzip v2ray-linux-64.zip -d v2ray && sudo cp v2ray/v2ray v2ray/v2ctl /usr/local/bin
+v2ray -c config.json
+export http_proxy="http://127.0.0.1:8001" https_proxy="http://127.0.0.1:8001"
+
+
+# Tmux & Tmux configuration
+sudo apt-get install -y libevent-dev ncurses-dev automake pkg-config \
+    && cd /tmp \
+    && wget -O tmux-2.8.tar.gz https://github.com/tmux/tmux/archive/2.8.tar.gz \
+    && tar zxvf tmux-2.8.tar.gz \
+    && cd tmux-2.8 \
+    && ./autogen.sh \
+    && ./configure --prefix=/usr/local \
+    && make \
+    && sudo make install
+git clone https://github.com/hughplay/tmux-config.git /tmp/tmux-config \
+    && bash /tmp/tmux-config/install.sh \
+    && rm -rf /tmp/tmux-config \
+    && echo "set -g default-shell `which zsh`" >> ~/.tmux.conf
+
+
+# ZSH configuration
+curl https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash -s -- --unattended
+wget https://raw.githubusercontent.com/oskarkrawczyk/honukai-iterm/master/honukai.zsh-theme -O ${ZSH:-~/.oh-my-zsh}/themes/honukai.zsh-theme --no-check-certificate \
+    && sed -i.bak '/ZSH_THEME/s/\".*\"/\"honukai\"/' ~/.zshrc \
+    && git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH:-~/.oh-my-zsh}/custom/plugins/zsh-autosuggestions\
+    && sed -i.bak '/plugin/s/(.*)/(git zsh-autosuggestions)/' ~/.zshrc
+# ZSH as default shell
+sudo chsh -s "$(command -v zsh)" "${USER}"
+
+
+# Install Nvidia Driver
+# https://developer.nvidia.com/cuda-downloads
+wget https://developer.download.nvidia.com/compute/cuda/11.6.2/local_installers/cuda_11.6.2_510.47.03_linux.run
+sudo sh cuda_11.6.2_510.47.03_linux.run
+"""
+===========
+= Summary =
+===========
+
+Driver:   Installed
+Toolkit:  Installed in /usr/local/cuda-11.6/
+
+Please make sure that
+ -   PATH includes /usr/local/cuda-11.6/bin
+ -   LD_LIBRARY_PATH includes /usr/local/cuda-11.6/lib64, or, add /usr/local/cuda-11.6/lib64 to /etc/ld.so.conf and run ldconfig as root
+
+To uninstall the CUDA Toolkit, run cuda-uninstaller in /usr/local/cuda-11.6/bin
+To uninstall the NVIDIA Driver, run nvidia-uninstall
+Logfile is /var/log/cuda-installer.log
+"""
+
+
+# Install Miniconda
+wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/install-conda.sh \
+    && chmod +x /tmp/install-conda.sh \
+    && /tmp/install-conda.sh -b -p $HOME/.miniconda \
+    && rm -f /tmp/install-conda.sh \
+    && export PATH="$HOME/.miniconda/bin:$PATH" \
+    && conda init \
+    && cat <<EOT >> ~/.condarc
+channels:
+  - defaults
+show_channel_urls: true
+channel_alias: https://mirrors.tuna.tsinghua.edu.cn/anaconda
+default_channels:
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/r
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/pro
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/msys2
+custom_channels:
+  conda-forge: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  msys2: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  bioconda: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  menpo: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  pytorch: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  simpleitk: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+EOT
+conda install -y python=3.8
+pip install gpustat
+sudo nvidia-smi daemon
+gpustat -i 0.3
+
+# Install docker
+curl https://get.docker.com | sh \
+  && sudo systemctl --now enable docker
+# https://docs.docker.com/engine/install/linux-postinstall/
+sudo usermod -aG docker $USER
+# relogin or run:
+newgrp docker
+# test
+docker run hello-world
+
+
+# Install nvidia-docker2
+# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt-get update && sudo apt-get install -y nvidia-docker2
+sudo systemctl restart docker
+# test
+sudo docker run --rm --gpus all nvidia/cuda:11.0-base nvidia-smi
+
