05515504fa809c3a938e9fb05d025815249efa5d
Mr.Blue <silverhugh.77@gmail.com>
2022-07-06 20:59:54 +0800

    Update notebook.py

--- code/snippet/drawing/notebook.py
+++ code/snippet/drawing/notebook.py
@@ -8,3 +8,10 @@ rm -fr ~/.cache/matplotlib
 # list all available fonts
 import matplotlib.font_manager
 print(sorted([f.name for f in matplotlib.font_manager.fontManager.ttflist]))
+
+
+# change style of matplotlib
+# -------
+# https://matplotlib.org/stable/tutorials/introductory/customizing.html#using-style-sheets
+# https://matplotlib.org/stable/gallery/style_sheets/style_sheets_reference.html
+plt.style.use('ggplot')
