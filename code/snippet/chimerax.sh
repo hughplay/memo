51d784eea7449f5a02d14aac6f43a6a342b79dc8
Mr.Blue <silverhugh.77@gmail.com>
2023-11-29 17:03:36 +0800

    Create chimerax.sh

---
+++ code/snippet/chimerax.sh
@@ -0,0 +1,8 @@
+# Visualization using ChimeraX
+
+## plot molecular
+# comands: https://www.cgl.ucsf.edu/chimerax/docs/user/tools/moldisplay.html
+style stick
+color #1.271 byelement transparency 70
+hide #1.1-361 target m
+show #1.241,271,301,331,361 target m
